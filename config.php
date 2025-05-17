f942ad0b9464f5d5ec4b5827f605639d76731809
ertu34 <ymgazazel@gmail.com>
2025-05-17 22:48:09 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
-$site_name  = "Cuymanga";                 // Nama situs
-$site_url   = "http://localhost:8030";    // URL domain utama
+$site_name  = "alicescanstrtest";                 // Nama situs
+$site_url   = "https://alicescanstrtest.netlify.app/";    // URL domain utama
 $url_api    = $site_url;                  // API URL (bisa disesuaikan jika terpisah)
 $image_kit  = "https://ik.imagekit.io//"; // Gunakan ImageKit sendiri, tuk bypass img chapter (daftar di https://imagekit.io)
 $image_og   = "$site_url/storage/og.png"; // url Banner website untuk Open Graph (OG)
